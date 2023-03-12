e548292bb8a8ecbb8efc9f1be1a81f525b8cdb0c
Akhilesh Yadav <45841446+AkhileshYadavWorld@users.noreply.github.com>
2023-03-12 16:27:06 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,39 @@
-const addResourcesToCache = async (resources) => {
-  const cache = await caches.open("v1");
-  await cache.addAll(resources);
-};
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    addResourcesToCache([
-      "/",
-      "/pwa.html",
-      "/app.js",
-    ])
-  );
+const CACHE_NAME = `temperature-converter-v1`;
+    
+// Use the install event to pre-cache all initial resources.
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll([
+      './',
+      './converter.js',
+      './converter.css'
+    ]);
+  })());
 });
-            
+
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+
+    // Get the resource from the cache.
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
+        try {
+          // If the resource was not in the cache, try the network.
+          const fetchResponse = await fetch(event.request);
+    
+          // Save the resource in the cache and return it.
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        } catch (e) {
+          // The network failed
+        }
+    }
+  })());
+});      
             
             
             
